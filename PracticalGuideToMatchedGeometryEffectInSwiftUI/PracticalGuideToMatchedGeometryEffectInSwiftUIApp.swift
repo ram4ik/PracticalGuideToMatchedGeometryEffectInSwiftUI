8ad9be3da487563dd87bf2e68b1b21970c84ceda
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-29 18:33:46 +0300

    Initial Commit

---
+++ PracticalGuideToMatchedGeometryEffectInSwiftUI/PracticalGuideToMatchedGeometryEffectInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalGuideToMatchedGeometryEffectInSwiftUIApp.swift
+//  PracticalGuideToMatchedGeometryEffectInSwiftUI
+//
+//  Created by ramil on 29.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct PracticalGuideToMatchedGeometryEffectInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
